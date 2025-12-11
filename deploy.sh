899ac1e91b801fd9fe32cb9bd644f2b8cf11800f
yao0525888 <125794338+yao0525888@users.noreply.github.com>
2025-12-11 19:20:22 +0800

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -121,7 +121,7 @@ if [ "$action" = "3" ]; then
 fi
 
 # 独立更新 default1.html
-if [ "$action" = "4" ]; then
+if [ "$action" = "5" ]; then
     echo ""
     echo "=========================================="
     echo "  更新 default1.html 文件"
